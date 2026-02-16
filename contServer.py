6813260d6e2fcc9bd159b2099f3c875a161caebc
Tarik366 <tariky113@gmail.com>
2026-02-16 19:12:58 +0300

    packet docs moved

--- contServer.py
+++ contServer.py
@@ -2,23 +2,6 @@ from socket import *
 import vgamepad as vg
 
 """
-Incoming packet:
-    pressed = 0b0000010000010000 # Right trigger and square pressing
-    specials = 0b01 # PS button pressing
-    dpad = 0b0000
-    l3x = 0.0
-    l3y = 1.0
-    r3x = 0.0
-    r3y = 0.0
-    # After this, this variables are optional
-    rt = 0x0
-    lt = 0x255
-    touchx: U16 = 0x861
-    touchy: U16 = 0x500
-    acx, acy, acz: U16 # Accelerometer
-    gyx, gyy, gyz: U16 # Gyroscope
-    microphone:PackedBytes # maybe??????
-
 Outcoming packet:
     rumble_large = [0,255]
     rumble_small = [0,255]
