08ace16c5f078bf0ef77907447f5f81a496ed3b9
Tarik366 <tariky113@gmail.com>
2026-02-17 05:57:15 +0300

    continious releases added

---
+++ docker/windows/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+mkdir /build
+cmake -S /cont -B /build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE=/mingw-w64-x86_64.cmake
+cmake --build /build --parallel
+cmake --install /build --prefix "/install"
+
+# HACK: Manual fixup, would be nice to not need this eventually
+cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll /install/bin/
