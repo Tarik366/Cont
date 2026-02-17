abaeca8b733987befa382c0ba0578a9f1bba2dfa
Tarik366 <tariky113@gmail.com>
2026-02-17 19:43:40 +0300

    Fixed adding static libraries

--- docker/windows/build.sh
+++ docker/windows/build.sh
@@ -9,3 +9,9 @@ cmake --install /build --prefix "/install"
 
 # HACK: Manual fixup, would be nice to not need this eventually
 cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll /install/bin/
+# Adding MinGW standart libraries 
+cp /usr/lib/gcc/x86_64-w64-mingw32/*-posix/libgcc_s_seh-1.dll /install/bin/ || true
+cp /usr/lib/gcc/x86_64-w64-mingw32/*-posix/libstdc++-6.dll /install/bin/ || true
+
+# Eğer SDL3 shared derlendiyse onu da bul ve kopyala
+find /build -name "SDL3.dll" -exec cp {} /install/bin/ \;
