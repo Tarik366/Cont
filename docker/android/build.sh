5afebbbfc7dcb11f4ff806b5b9240d49945ec0b9
Tarik366 <tariky113@gmail.com>
2026-02-23 01:38:50 +0300

    Android Build

---
+++ docker/android/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+cd /cont/android
+./gradlew -PbuildDir=/build assembleRelease
+zipalign -v -p 4 /build/outputs/apk/release/app-release-unsigned.apk /build/outputs/apk/release/app-release-unsigned-aligned.apk
+
+# If signing key is available, sign the APK
+if [ -n "$ANDROID_SIGNING_KEY" ]
+then
+    apksigner sign --ks /cont/docker/android/cont.jks --out /install/app-release.apk --ks-pass pass:$ANDROID_SIGNING_KEY /build/outputs/apk/release/app-release-unsigned-aligned.apk
+else
+    # Otherwise, copy into the location expected by other scripts
+    cp /build/outputs/apk/release/app-release-unsigned-aligned.apk /install/app-release.apk
+fi
