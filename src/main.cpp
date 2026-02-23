1525cd59f09f09037b4766704e6b0e70d260194f
Tarik366 <tariky113@gmail.com>
2026-02-23 05:32:46 +0300

    I want to sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-// Dear ImGui: standalone example application for SDL3 + OpenGL
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
+// I lose my mind
 
 #include <imgui.h>
 #include <imgui_impl_sdl3.h>
